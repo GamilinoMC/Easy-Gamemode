87448861c3be1d6336f91d00fdea403b85c66ebf
GamilinoMC <81467242+GamilinoMC@users.noreply.github.com>
2022-05-04 15:39:23 +0200

    Create Main.php

--- src/Plugin/Main.php
+++ src/Plugin/Main.php
@@ -1,1 +1,15 @@
-<php?
+<?php
+
+namespace Plugin;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase{
+
+   public function onCommand(CommandSender $sender, Command $cmd, string $lable, array $args): bool{
+       if($cmd->getName() --- "test"){
+           
+     }
+}
