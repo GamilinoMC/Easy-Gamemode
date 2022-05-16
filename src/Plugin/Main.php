830c37f0d92646ed3f67b41d35eab087686c00ee
GamilinoMC <81467242+GamilinoMC@users.noreply.github.com>
2022-05-16 10:37:34 +0200

    Update Main.php

--- src/Plugin/Main.php
+++ src/Plugin/Main.php
@@ -6,7 +6,7 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\plugin\PluginBase;
 
-class Main extends PluginBase{
+class Main.php extends PluginBase{
 
     public function onCommand(CommandSender $sender, Command $cmd, string $lable, array $args): bool{
         if($cmd->getName() --- "test"){
